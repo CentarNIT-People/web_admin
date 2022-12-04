7a79dfc0da9f23fa05f6325056ea72f1650c953c
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-12-04 12:16:18 +0100

    refactoring

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -1,6 +1,4 @@
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import { AppNavbar } from "./Navbar/Navbar";
-import { Header } from "./Header/Header";
 import { useStyles } from "./Home/HomeStyles";
 import { Home } from "./Home/Home";
 
@@ -9,8 +7,6 @@ export const AppRoutes = () => {
     return (
         <BrowserRouter>
             <div className={classes.app}>
-                <AppNavbar />
-                <Header />
                 <Routes>
                     <Route path="/" element={<Outlet />}>
                         <Route index element={<Home />} />
